f9797795c434268a018793be672828a5d060935b
Keyvan Kkhademi Ravandi <keyvan.khademiravandi@student.kdg.be>
2021-03-03 18:28:49 +0100

    made script more platform independent by using os seperator

--- exercise01.py
+++ exercise01.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os
 path = os.getcwd()
-path = path.strip('/')
-path_components = path.split('/')
+path = path.strip(os.sep)
+path_components = path.split(os.sep)
 print(path_components)
