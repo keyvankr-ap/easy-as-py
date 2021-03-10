7e5e4aa4be0ba3204aefcda5132cd0cca9634c1b
Keyvan Kkhademi Ravandi <keyvan.khademiravandi@student.kdg.be>
2021-03-10 18:42:52 +0100

    created new function for length and optional argument
    created a new function to check the length of the path, also made sure that the path argument in the function is optional and also checked whenever the function is called upon

--- exercise01.py
+++ exercise01.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
 
 import os
-def get_path_components(path):
+def get_path_components(path=None):
     #path = os.getcwd()
     #path = path.strip(os.sep)
+    if path is None:
+        path = os.getcwd()
+    
     path_components = path.split(os.sep)
+
+    
     while '' in path_components :
         path_components.remove('')
     return path_components
 
+def get_path_length(path=None):
+    return len(get_path_components(path))
 
-print(get_path_components('/usr/local/bin/'))
+
+
+print(get_path_components())
+print(get_path_length("/usr/bin"))
