aadd299d2a6db59b45732fd266dfbdfedc166e3c
Keyvan Kkhademi Ravandi <keyvan.khademiravandi@student.kdg.be>
2021-03-10 18:28:41 +0100

    added parameter for the newly created function

--- exercise01.py
+++ exercise01.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
 import os
-def get_path_components():
-    path = os.getcwd()
-    path = path.strip(os.sep)
+def get_path_components(path):
+    #path = os.getcwd()
+    #path = path.strip(os.sep)
     path_components = path.split(os.sep)
     while '' in path_components :
         path_components.remove('')
     return path_components
 
 
-print(get_path_components())
+print(get_path_components('/usr/local/bin/'))
